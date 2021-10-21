8c7b9611e097f92d58e5d2a3169075346acc8676
Laxmikanth1485 <klaxmikanth97@gmail.com>
2021-10-21 19:35:37 +0530

    cherry

--- newfile.sh
+++ newfile.sh
@@ -3,10 +3,9 @@ echo "we are learning so many things these days"
 echo "which is very interesting thing in life"
 echo "don't worry"
 echo "Hello Good Morning"
-
 echo "Hello world"
 echo "same thing"
 echo "think before you speak"
 updated
 echo "same thing"
-
+cherry-pick
